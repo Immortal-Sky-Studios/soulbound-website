a2eea0433432f9f11eb0a90e4b9a3f54a3d32015
gracieArmour <graciearmour42@gmail.com>
2025-03-04 03:02:19 -0800

    Nav bar skeleton

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import Image from "next/image";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +29,32 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header className="flex flex-row justify-between">
+          <div className="flex flex-row">
+            <a
+              href="/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/soulboundName.svg"
+                alt="Soulbound logo"
+                width={20}
+                height={20}
+              />
+            </a>
+          </div>
+
+          <div className="flex flex-row-reverse">
+
+          </div>
+        </header>
+        
         {children}
+
+        <footer>
+
+        </footer>
       </body>
     </html>
   );
