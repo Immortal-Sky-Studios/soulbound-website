365cb30fafffff3030e5ab429958ddcf68ad922b
gracieArmour <graciearmour42@gmail.com>
2025-03-04 00:36:44 -0800

    Initial Soulbound site structure pages

---
+++ app/episodes/[slug]/page.tsx
@@ -0,0 +1,10 @@
+export default async function EpisodeDynamic({
+        params,
+    }: {
+        params: Promise<{ slug: string }>
+    }) {
+        const { slug } = await params
+        return (
+            <div>My Post: {slug}</div>
+        )
+}
