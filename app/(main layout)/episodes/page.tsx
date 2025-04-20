d72961bbcadf08c87920629b647272d18678bcbd
gracieArmour <graciearmour42@gmail.com>
2025-04-20 03:52:20 -0700

    Started styling Episodes page

--- app/(main layout)/episodes/page.tsx
+++ app/(main layout)/episodes/page.tsx
@@ -8,26 +8,30 @@ export default function Episodes() {
     const showLinks = getShowLinks('Podcast Hosts');
 
     return (
-        <Suspense fallback={
-            <main className="flex flex-row justify-items-center w-screen bg-[/StarryBG.gif]">
-                <div id="center-container" className="flex flex-col w-0.5 h-full">
-                    <div id="main-header" className="flex flex-col">
-                        <h1>EPISODES</h1>
-                        <ul className="flex flex-row w-2/3 px-[2em] justify-between align-center list-none">
-                            <li>Loading...</li>
-                        </ul>
-                        <hr/>
-                        <div id="search-container">
-                            <input name="episode-search" type="text" placeholder="Loading..." defaultValue="" autoFocus={true} />
+        <main className="flex flex-row justify-center w-screen bg-[url('/backgrounds/SpaceBannerSTATIC.png')]">
+            <Suspense fallback={
+                <div id="center-container" className="flex flex-col align-center w-1/2 h-full bg-background">
+                    <div id="main-header" className="flex flex-col align-center">
+                        <div className="flex flex-row justify-center">
+                            <h1 className="w-2/3 text-center">EPISODES</h1>
+                        </div>
+                        <div className="flex flex-row justify-center">
+                            <ul className="flex flex-row w-2/3 px-[2em] justify-between align-center list-none">
+                                <li>Loading...</li>
+                            </ul>
+                        </div>
+                        <hr className="my-[1em]"/>
+                        <div id="search-container" className="flex flex-row justify-center mb-[1em]">
+                            <input className="w-2/3" name="episode-search" type="text" placeholder="Loading..." defaultValue="" autoFocus={true} />
                         </div>
                     </div>
                     <ul id="results-list" className="overflow-scroll list-none">
                         Loading...
                     </ul>
                 </div>
-            </main>
-        }>
-            <EpisodesList episodes={episodes} links={showLinks}/>
-        </Suspense>
+            }>
+                <EpisodesList episodes={episodes} links={showLinks}/>
+            </Suspense>
+        </main>
     )
 }
--- components/EpisodesList.tsx
+++ components/EpisodesList.tsx
@@ -27,28 +27,30 @@ export default function EmbeddedEpisode({
     }
 
     return (
-        <main className="flex flex-row justify-items-center w-screen bg-[/StarryBG.gif]">
-                <div id="center-container" className="flex flex-col w-1/2 h-full">
-                    <div id="main-header" className="flex flex-col">
-                        <h1>EPISODES</h1>
-                        <ShowLinks links={links} />
-                        <hr/>
-                        <div id="search-container">
-                            <input name="episode-search" type="text" placeholder="Search for an episode" defaultValue="" autoFocus={true} onChange={handleSearch} />
-                        </div>
-                    </div>
-                    <ul id="results-list" className="flex flex-col overflow-scroll list-none">
-                        {filteredEps.map((entry) => {
-                            return (
-                                <li key={entry.id} className="flex flex-row">
-                                    <div>Season {entry.season_num}</div>
-                                    <h2><Link href={`/episodes/${entry.slug}`}>Ep {entry.ep_num} {entry.title}</Link></h2>
-                                </li>
-                            )
-                        })}
-                    </ul>
+        <div id="center-container" className="flex flex-col align-center w-1/2 h-full bg-background">
+            <div id="main-header" className="flex flex-col align-center">
+                <div className="flex flex-row justify-center">
+                    <h1 className="w-2/3 text-center">EPISODES</h1>
                 </div>
-            </main>
+                <div className="flex flex-row justify-center">
+                    <ShowLinks links={links} />
+                </div>
+                <hr className="my-[1em]"/>
+                <div id="search-container" className="flex flex-row justify-center mb-[1em]">
+                    <input className="w-2/3" name="episode-search" type="text" placeholder="Search for an episode" defaultValue="" autoFocus={true} onChange={handleSearch} />
+                </div>
+            </div>
+            <ul id="results-list" className="flex flex-col overflow-scroll list-none">
+                {filteredEps.map((entry) => {
+                    return (
+                        <li key={entry.id} className="flex flex-row">
+                            <div>Season {entry.season_num}</div>
+                            <h2><Link href={`/episodes/${entry.slug}`}>Ep {entry.ep_num} {entry.title}</Link></h2>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
     )
 }
 
