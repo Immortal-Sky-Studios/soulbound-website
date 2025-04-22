d57d19e283d5c98daa9877aebcba144e799a4935
gracieArmour <graciearmour42@gmail.com>
2025-04-21 19:48:00 -0700

    Added dynamic robots.txt

---
+++ app/robots.ts
@@ -0,0 +1,21 @@
+import type { MetadataRoute } from 'next'
+import fs from 'fs';
+
+export default async function robots(): Promise<MetadataRoute.Robots> {
+    const botsStr = fs.readFileSync('lib/releaseDownloads/robots.txt').toString();
+    const aiBots = botsStr.split('\n').map((entry) => entry.split('User-agent: ')[1]).filter((entry) => (entry));
+
+    return {
+        rules: [
+        {
+            userAgent: aiBots,
+            disallow: ['/'],
+        },
+        {
+            userAgent: '*',
+            allow: ['/'],
+        },
+        ],
+        sitemap: 'https://acme.com/sitemap.xml',
+    }
+}
--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import './ai.robots.ts';
 
 const nextConfig: NextConfig = {
   /* config options here */
