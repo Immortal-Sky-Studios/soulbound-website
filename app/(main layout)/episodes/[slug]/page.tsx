ecedcdf57c30a5bb1189dfcd5013b145eed2965a
gracieArmour <graciearmour42@gmail.com>
2025-07-17 15:36:32 -0700

    Major episode slug styling progress

--- app/(main layout)/episodes/[slug]/page.tsx
+++ app/(main layout)/episodes/[slug]/page.tsx
@@ -27,12 +27,13 @@ export default async function EpisodeDynamic({
     }) {
         const { slug } = await params;
         const data = await getEpisode(slug);
+        const creditsOffset = data.credits.filter((entry) => entry.superrole === 'Cast').length + 2;
         const spotifyComponents = data.link_spotify.split('episode');
 
         return (
-            <main className="p-[2em]">
+            <main className="min-h-screen p-[2rem]">
                 <div id="top-section" className="flex flex-col sm:flex-row justify-center">
-                    <div id="left-section" className="mx-[1em]">
+                    <div id="left-section" className="mx-[1rem]">
                         <Image
                             id="episode-cover"
                             className="h-[300px] w-auto"
@@ -42,7 +43,7 @@ export default async function EpisodeDynamic({
                             height={2048}
                             priority
                         />
-                        <iframe className="w-[300px] h-[152px] my-[1em]" src={`${spotifyComponents[0]}embed/episode/${spotifyComponents[1]}?utm_source=generator`} width="100%" height="352" allowFullScreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                        <iframe className="w-[300px] h-[152px] my-[1rem]" src={`${spotifyComponents[0]}embed/episode/${spotifyComponents[1]}?utm_source=generator`} width="100%" height="352" allowFullScreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                         <ul id="showlinks-container" className="flex flex-row justify-around align-center">
                             <li>
                                 <Link href={data.link_spotify} target="_blank" rel="noopener noreferrer">
@@ -76,35 +77,50 @@ export default async function EpisodeDynamic({
                             </li>
                         </ul>
                     </div>
-                    <div id="right-section">
+                    <div id="right-section" className="flex flex-col align-start">
                         <div id="episode-info-container" className="flex flex-col align-start">
-                            <h2 className="w-1/3 border-2 mask-clip-border mask-[url(/images/masks/season-mask.svg)]">Season {data.season_num}</h2>
-                            <div id="episode-info">
+                            <div className="flex flex-row justify-between">
+                                <h2 className="w-1/3 px-[1rrem] border-2 mask-clip-border">Season {data.season_num}</h2> {/* mask-[url(/images/masks/season-mask.svg)] */}
+                                <div id="prev-next-container" className="flex flex-row align-center pb-[2rem]">
+                                    <button className="p-[0.5rem]">Prev</button>
+                                    <button className="p-[0.5rem]">Next</button>
+                                </div>
+                            </div>
+                            <div id="episode-info" className="border-2 -mt-[1rem] p-[1rem]">
                                 <h2>Soulbound Ep.{data.season_ep_num} - {data.title}</h2>
                                 <p id="episode-description">{data.description}</p>
                                 <p id="trigger-warnings">Trigger Warnings: {data.triggers}</p>
                             </div>
                         </div>
-                        <div id="transcript-container">
+                        <div id="transcript-container" className="border-2 mt-[1rem] p-[1rem] grow">
                             However the fuck we render the transcript
                         </div>
                     </div>
                 </div>
-                <div id="credits-container">
-                    {data.credits.filter((entry) => entry.superrole === 'cast').map((entry,index) => {
-                        return (
-                            <div key={index}>
-                                <b>{entry.role}:</b> {entry.name}
-                            </div>
-                        )
-                    })}
-                    {data.credits.filter((entry) => entry.superrole === 'crew').map((entry,index) => {
-                        return (
-                            <div key={index}>
-                                <b>{entry.role}:</b> {entry.name}
-                            </div>
-                        )
-                    })}
+                <div id="credits-container" className="border-2 mt-[1rem] p-[1rem]">
+                    <h2 className="flex flex-row justify-center">Cast</h2>
+                    <div className="grid grid-cols-2 align-start justify-center">
+                        {data.credits.filter((entry) => entry.superrole === 'Cast').map((entry,index) => {
+                            return (
+                                <>
+                                    <div key={2*index} className="flex flex-row-reverse mx-[1rem]"><b>{entry.role}:</b></div>
+                                    <div key={(2*index)+1}>{entry.name}</div>
+                                </>
+                            )
+                        })}
+                    </div>
+
+                    <h2 className="flex flex-row justify-center mt-[1rem]">Crew</h2>
+                    <div className="grid grid-cols-2 align-start justify-center">
+                        {data.credits.filter((entry) => entry.superrole === 'Crew').map((entry,index) => {
+                            return (
+                                <>
+                                    <div key={2*(index+creditsOffset)} className="flex flex-row-reverse mx-[1rem]"><b>{entry.role}:</b></div>
+                                    <div key={(2*(index+creditsOffset))+1}>{entry.name}</div>
+                                </>
+                            )
+                        })}
+                    </div>
                 </div>
             </main>
         )
