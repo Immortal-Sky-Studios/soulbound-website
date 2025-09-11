e08899dd01d5acd8817826ba1130ce98bce448a3
Gracie Armour <43764940+gracieArmour@users.noreply.github.com>
2025-09-10 17:06:53 -0700

    Added border images to episodes page, cleaned up styling

--- app/(main layout)/episodes/page.tsx
+++ app/(main layout)/episodes/page.tsx
@@ -23,10 +23,10 @@ export default function Episodes() {
 
     return (
         <main className="flex flex-row justify-center w-full bg-[url('/images/backgrounds/SpaceBannerSTATIC.png')]">
-            <div id="center-container" className="flex flex-col w-full md:w-1/2 h-full bg-background">
+            <div id="center-container" className="flex flex-col w-full md:w-1/2 h-full bg-background border-x-[1rem] border-image-[url('/images/borders/RivetSideBorder.png')] border-slice-[157] border-image-width-[1rem] border-repeat-round">
                 <div id="main-header" className="flex flex-col">
                     <div className="flex flex-row justify-center">
-                        <h1 className={`${stalinistOne.className} w-2/3 text-center mt-[0.25rem]`}>EPISODES</h1>
+                        <h1 className={`${stalinistOne.className} w-fit px-[1rem] text-center mt-[0.5rem] border-[1rem] border-image-[url('/images/borders/RivetCornerBorder.png')] border-slice-[157] border-image-width-[1rem] border-repeat-round`}>EPISODES</h1>
                     </div>
                     <div className="flex flex-row justify-center">
                         <ShowLinksList links={showLinks} />
--- components/EpisodesList.tsx
+++ components/EpisodesList.tsx
@@ -33,13 +33,13 @@ export default function EpisodesList({
                     height={15}
                     className="-mr-[1.25rem]"
                 />
-                <input className="px-[1.5rem] w-2/3 border-2" name="episode-search" type="text" placeholder="Search for an episode" defaultValue="" autoFocus={true} onChange={handleSearch} />
+                <input className="px-[1.5rem] w-2/3 border-2 border-[#58595b]" name="episode-search" type="text" placeholder="Search for an episode" defaultValue="" autoFocus={true} onChange={handleSearch} />
             </div>
-            <ul id="results-list" className="flex flex-col overflow-y-auto list-none h-[75vh]">
+            <ul id="results-list" className="flex flex-col overflow-y-auto list-none h-[75vh] border-t-1">
                 {!filteredEps?.length ? <Skeleton count={10}/> : filteredEps.map((entry) => {
                     return (
                         <li key={entry.id} className="flex flex-row border">
-                            <div className="flex flex-col justify-center items-center text-center h-1/2 m-[1rem] p-[0.25rem] border-2">Season {entry.season_num}</div>
+                            <div className="flex flex-col justify-center items-center text-center h-1/2 m-[1rem] p-[0.25rem] border-2 border-[#58595b]">Season {entry.season_num}</div>
                             <Link href={`/episodes/${entry.slug}`} className="flex flex-col justify-center items-center text-3xl font-medium">{entry.season_ep_num && `Ep ${entry.season_ep_num}`} {entry.title}</Link>
                         </li>
                     )
