8c7baba9ab62c8f2bfc1fe47d96f813a11f36861
Gracie Armour <43764940+gracieArmour@users.noreply.github.com>
2025-09-17 14:27:05 -0700

    Upgraded episode search to allow searching by ep number

--- components/EpisodesList.tsx
+++ components/EpisodesList.tsx
@@ -16,7 +16,7 @@ export default function EpisodesList({
 
     const [searchItem, setSearchItem] = useState('');
     const filteredEps = episodesList.filter((entry) => 
-            entry.title.toLowerCase().includes(searchItem.toLowerCase())
+            entry.title.toLowerCase().includes(searchItem.toLowerCase()) || entry.season_ep_num == Number(searchItem)
         );
 
     const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
@@ -31,16 +31,18 @@ export default function EpisodesList({
                     alt="Search magnifying glass icon"
                     width={15}
                     height={15}
-                    className="-mr-[1.25rem]"
+                    className="-mr-[1.25rem] z-2"
                 />
-                <input className="px-[1.5rem] w-2/3 border-2 border-[#58595b]" name="episode-search" type="text" placeholder="Search for an episode" defaultValue="" autoFocus={true} onChange={handleSearch} />
+                <input className="px-[1.5rem] w-2/3 border-2 border-[#58595b] bg-cyan-300" name="episode-search" type="text" placeholder="Search by title or episode number" defaultValue="" autoFocus={true} onChange={handleSearch} />
             </div>
             <ul id="results-list" className="flex flex-col overflow-y-auto list-none h-[75vh] border-t-1">
                 {!filteredEps?.length ? <Skeleton count={10}/> : filteredEps.map((entry) => {
                     return (
-                        <li key={entry.id} className="flex flex-row border">
-                            <div className="flex flex-col justify-center items-center text-center h-1/2 m-[1rem] p-[0.25rem] border-2 border-[#58595b]">Season {entry.season_num}</div>
-                            <Link href={`/episodes/${entry.slug}`} className="flex flex-col justify-center items-center text-3xl font-medium">{entry.season_ep_num && `Ep ${entry.season_ep_num}`} {entry.title}</Link>
+                        <li key={entry.id} className="border bg-cyan-300">
+                            <Link href={`/episodes/${entry.slug}`} className="flex flex-row w-full h-full">
+                                <div className="flex flex-col justify-center items-center text-center h-1/2 m-[1rem] p-[0.25rem] border-2 border-[#58595b]">Season {entry.season_num}</div>
+                                <div className="flex flex-col justify-center items-center text-3xl font-medium">{entry.season_ep_num && `Ep ${entry.season_ep_num}`} {entry.title}</div>
+                            </Link>
                         </li>
                     )
                 })}
