9160d5fe1581c671efd1711d28d12ae1a3b87a47
gracieArmour <graciearmour42@gmail.com>
2025-11-05 00:56:53 -0800

    Added new front page text

--- app/(main layout)/page.tsx
+++ app/(main layout)/page.tsx
@@ -7,6 +7,7 @@ import defaultCover from "@images/covers/episodes/defaultCover.jpg"
 import EmbeddedEpisode from "@components/EmbeddedEpisode";
 import ShowLinks from "@components/ShowLinks";
 import { getLatestEp, getShowLinks } from "@lib/neonFunctions";
+import Link from "next/link";
 
 export default async function Home() {
     const latestEp = getLatestEp();
@@ -25,11 +26,14 @@ export default async function Home() {
             <div id="whatis-section" className="flex flex-col sm:flex-row items-center justify-around py-[2rem] px-[2rem] sm:px-[10rem] bg-cyan-300 border-[2rem] border-image-[url('/images/borders/MetalScrewBorder.png')] border-slice-[990] border-image-width-[150px] border-repeat-round">
                 <div id="podcast-description" className="flex flex-col justify-center sm:w-1/3">
                     <h2>What is Soulbound?</h2>
-                    Doctor Jaylin Glaslow is a well renowned historian aboard a space station known as the Deck. When he learns of robots who have begun to think and feel without the aid of AI or manmade devices, he is sent out to investigate why these robots have developed sentience through interviewing them on a desolate planet known as Last Stand, a place uninhabitable by organic life, but a great place for inorganic life to hide. Every week he interviews a new robot to learn about their planet and their past, hoping that maybe one day he’ll figure out what makes them work the way they do.
+                    When robots across the known galaxy begin to develop sentience, the Astral Assembly sends their most disposable highest ranked historical officer, Dr Jaylin Glaslow, to investigate. When he&apos;s not conducting interviews and prying into the personal lives of his robotic hosts, Dr Glaslow spends his time learning more about political history and editing screenplays.
+                    In other words, Soulbound is a Science Fiction Audio Drama Podcast about sentient robots and what makes us truly human. Or, hominid. Or, Fleshbag. Depending on who you ask. We&apos;re published under a Creative Commons Attribution Noncommercial International License, which means you&apos;re free to repost audio from our podcast and make fan content so long as you don&apos;t claim you made the podcast itself and you don&apos;t profit off the work. 
+                    We ask you not use any of our audio, scripts, or art to train any form of artificial intelligence. Despite it&apos;s subject matter, we never use Artificial Intelligence to make any of our content. 100% human made content, that&apos;s our guarantee.
+                    If you&apos;d like to get in touch with us, feel free to reach out to us at <Link className="underline" href="mailto:thesoulboundseries@gmail.com" target="_blank">thesoulboundseries@gmail.com</Link>
                 </div>
                 <Image
                     id="episode-cover"
-                    className="h-[20rem] w-auto object-contain m-[2rem] rounded-lg"
+                    className="h-[25rem] w-auto object-contain m-[2rem] rounded-lg"
                     src={defaultCover}
                     alt="Soulbound Podcast cover art"
                 />
