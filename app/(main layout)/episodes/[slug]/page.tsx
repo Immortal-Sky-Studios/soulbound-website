bdf4619bf578adbb36965de6c30ffb97c3d14548
gracieArmour <graciearmour42@gmail.com>
2026-02-11 11:06:19 -0800

    Breakpoint adjustment to Episode pages

--- app/(main layout)/episodes/[slug]/page.tsx
+++ app/(main layout)/episodes/[slug]/page.tsx
@@ -62,27 +62,29 @@ export default async function EpisodeDynamic({
 
         return (
             <main className="min-h-screen w-full p-[2rem]">
-                <div id="top-section" className="flex flex-col sm:flex-row justify-center">
-                    <div id="left-section" className="flex flex-col items-center sm:items-start sm:mx-[1rem]">
+                <div id="top-section" className="flex flex-col md:flex-row justify-center">
+                    <div id="left-section" className="flex flex-col sm:max-md:flex-row items-center md:items-start md:mx-[1rem]">
                         <Image
                             id="episode-cover"
-                            className="h-[300px] w-auto"
+                            className="h-[300px] w-auto sm:max-md:w-1/2 sm:max-md:h-auto"
                             src={`/images/covers/episodes/${data.cover_filename}`}
                             alt={data.cover_alt_text}
                             width={data.cover_width}
                             height={data.cover_height}
                             priority
                         />
-                        <iframe className="w-[300px] h-[152px] my-[1rem]" src={`${spotifyComponents[0]}embed/episode/${spotifyComponents[1]}?utm_source=generator`} width="100%" height="352" allowFullScreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-                        <ul id="showlinks-container" className="flex flex-row justify-around items-center w-full mb-[1rem] sm:mb-0">
-                            <EpisodeLinks links={[data.link_spotify,data.link_apple,data.link_amazon]}/>
-                        </ul>
-                        {/* <p>{'<iframe style="border-radius:12px;overflow:hidden" src="https://thesoulboundseries.com/embed/episodes/{slug}" width="100%" height="300" frameBorder="0" loading="lazy"></iframe>'}</p> */}
+                        <div className="flex flex-col items-center max-md:mx-[1rem]">
+                            <iframe className="w-[300px] h-[152px] my-[1rem]" src={`${spotifyComponents[0]}embed/episode/${spotifyComponents[1]}?utm_source=generator`} width="100%" height="352" allowFullScreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                            <ul id="showlinks-container" className="flex flex-row justify-around items-center w-full mb-[1rem] sm:mb-0">
+                                <EpisodeLinks links={[data.link_spotify,data.link_apple,data.link_amazon]}/>
+                            </ul>
+                            {/* <p>{'<iframe style="border-radius:12px;overflow:hidden" src="https://thesoulboundseries.com/embed/episodes/{slug}" width="100%" height="300" frameBorder="0" loading="lazy"></iframe>'}</p> */}
+                        </div>
                     </div>
                     <div id="right-section" className="flex flex-col items-stretch align-start grow">
                         <div id="episode-info-container" className="flex flex-col">
                             <div className="flex flex-row justify-between">
-                                <h2 className="w-1/3 px-[1rem] bg-cyan-300 border-x-4 border-t-4 border-[#414042] mask-clip-border mask-[url(/images/masks/RightAngledMask.png)]">Season {data.season_num}</h2> {/* mask-[url(/images/masks/season-mask.svg)] */}
+                                <h2 className="w-1/2 lg:w-1/3 px-[1rem] bg-cyan-300 border-x-4 border-t-4 border-[#414042]">Season {data.season_num}</h2> {/* mask-[url(/images/masks/season-mask.svg)] */}
                                 <div id="prev-next-container" className="pt-[0.5em]">
                                     {prevEp && <Link href={`/episodes/${prevEp?.slug}`} className="h-min px-[1em] py-[0.25rem] bg-cyan-300 border border-[#414042]">{'<'} Prev</Link>}
                                     {nextEp && <Link href={`/episodes/${nextEp?.slug}`} className="h-min px-[1em] py-[0.25rem] bg-cyan-300 border border-[#414042]">Next {'>'}</Link>}
@@ -102,33 +104,37 @@ export default async function EpisodeDynamic({
                         </div>
                     </div>
                 </div>
-                <div id="credits-container" className="flex flex-col md:flex-row justify-around items-center mt-[1rem] p-[1rem] bg-cyan-300 text-blue-700 border-[1rem] border-image-[url('/images/borders/ThreeRivetBorder.png')] border-slice-[180] border-image-width-[1rem] border-repeat-round">
-                    <div id="cast-container">
+                <div id="credits-container" className="flex flex-col md:flex-row justify-around items-center min-h-[15rem] mt-[1rem] p-[1rem] bg-cyan-300 text-blue-700 border-[1rem] border-image-[url('/images/borders/ThreeRivetBorder.png')] border-slice-[180] border-image-width-[1rem] border-repeat-round">
+                    <div id="cast-container" className="max-md:mb-[1rem]">
                         <h2 className="text-center">Cast</h2>
-                        <div className="grid grid-cols-2 align-start">
-                            {data.credits.filter((entry) => entry.superrole === 'Cast').map((entry) => {
-                                return (
-                                    <Fragment key={entry.id}>
-                                        <div className="flex flex-row-reverse mx-[1rem]"><b>{entry.role}:</b></div>
-                                        <div>{entry.name}</div>
-                                    </Fragment>
-                                )
-                            })}
-                        </div>
+                        {data.credits.length ? 
+                            <div className="grid grid-cols-2 align-start">
+                                {data.credits.filter((entry) => entry.superrole === 'Cast').map((entry) => {
+                                    return (
+                                        <Fragment key={entry.id}>
+                                            <div className="flex flex-row-reverse mx-[1rem]"><b>{entry.role}:</b></div>
+                                            <div>{entry.name}</div>
+                                        </Fragment>
+                                    )
+                                })}
+                            </div>
+                        : "Coming Soon..."}
                     </div>
-                    <hr className="w-1/7 rotate-90"/>
-                    <div id="crew-container">
+                    <hr className="w-[10rem] md:mr-[5rem] md:rotate-90"/>
+                    <div id="crew-container" className="max-md:mt-[0.5rem]">
                         <h2 className="text-center">Crew</h2>
-                        <div className="grid grid-cols-2 align-start">
-                            {data.credits.filter((entry) => entry.superrole === 'Crew').map((entry) => {
-                                return (
-                                    <Fragment key={entry.id}>
-                                        <div className="flex flex-row-reverse mx-[1rem]"><b>{entry.role}:</b></div>
-                                        <div>{entry.name}</div>
-                                    </Fragment>
-                                )
-                            })}
-                        </div>
+                        {data.credits.length ? 
+                            <div className="grid grid-cols-2 align-start">
+                                {data.credits.filter((entry) => entry.superrole === 'Crew').map((entry) => {
+                                    return (
+                                        <Fragment key={entry.id}>
+                                            <div className="flex flex-row-reverse mx-[1rem]"><b>{entry.role}:</b></div>
+                                            <div>{entry.name}</div>
+                                        </Fragment>
+                                    )
+                                })}
+                            </div>
+                        : "Coming Soon..."}
                     </div>
                 </div>
             </main>
