993b9436eae90b05db1bb8ce6c1e96b2ef0ee79a
gracieArmour <graciearmour42@gmail.com>
2026-02-11 17:01:57 -0800

    Fixed paragraph breaks across Home and About Us pages

--- app/(main layout)/about-us/page.tsx
+++ app/(main layout)/about-us/page.tsx
@@ -44,13 +44,13 @@ export default function AboutUs() {
                 </div>
                 <p className="text-center lg:w-2/3">
                     Hi, Olivia Jeske here, Type 1 Diabetic and creator of Soulbound. For those of you who haven&apos;t figured it out yet, Soulbound is a show about characters with disabilities fighting for their own survival. It started with three monologues I wrote in my free time to vent about different aspects of my own disability, those being the monologues of Maybelle, Null, and Buttons, and it spiraled from there. 
-                    <br/>
+                    <br/><br/>
                     Soulbound is about robots because that&apos;s all the representation I had. As far as disabled characters in media goes, there aren&apos;t many with chronic conditions that are dependent on prescription medications. Hell, the best type 1 diabetic representation out there is in South Park. Robots in science fiction have always been the closest thing out there to good ace and disabled rep, and they&apos;re always poorly written side characters who&apos;s sole narrative revolves around whether or not they&apos;re really people at all. That always hurt.
-                    <br/>
+                    <br/><br/>
                     In March of 2024, I was tasked with creating a podcast for a college course, I scrounged up those three monologues, developed them into full characters, threw in some old OC&apos;s, and dragged my best friend from high school and my girlfriend into a recording booth to do silly robot voices together. We published our first episode a few days after my 9 year diagnosis anniversary.
-                    <br/>
+                    <br/><br/>
                     Over time, I dragged more and more people into it. Friends, Acquaintances, Dungeon Masters, and those who just wanted to tell a good story. And now... here we are. A veritable volunteer army. I have the honor of editing together our voices to help tell a story I was never told. I get to create the representation I never had. I get to write about depressed characters who get to be happy, characters with chronic pain who get to be seen and treated, and I can edit the clicking of my insulin pump into the podcast and make you all realize how annoying it is to hear constantly! But really, I&apos;m just lucky everyone has been willing to listen.
-                    <br/>
+                    <br/><br/>
                     I hope you have good luck out there too.
                 </p>
             </div>
