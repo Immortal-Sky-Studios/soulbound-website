32435b21ac27402ac6eb17075479544b740b1496
gracieArmour <graciearmour42@gmail.com>
2026-03-10 17:27:34 -0700

    Finishing touches on Error page styling

--- app/(main layout)/error.tsx
+++ app/(main layout)/error.tsx
@@ -18,12 +18,12 @@ export default function Error({
     }, [error])
     
     return (
-        <main className="flex flex-col justify-center items-center h-[90vh] w-[100vw] bg-[url('/images/backgrounds/CrackedPageBackground.png')] bg-cover">
+        <main className="flex flex-col justify-center items-center h-[90vh] w-[100vw] p-[1rem] bg-[url('/images/backgrounds/CrackedPageBackground.png')] bg-cover">
             <h1 className="mb-0">DECK 0S REPORT: {<UserDateTime/>}</h1>
             <h1>SERVER ERROR</h1>
-            <h2>{error.message}{error.digest ? ` (${error.digest})` : ''}</h2>
+            <p className="text-[2rem] text-center mb-[1rem]">{error.message}{error.digest ? ` (${error.digest})` : ''}</p>
             <p className="mb-[2rem]">The Diamonds are working on it</p>
-            <button onClick={() => reset()}>Try Again</button>
+            <button onClick={() => reset()} className="border-2 rounded bg-cyan-300 px-[0.5rem]">Try Again</button>
         </main>
     )
 }
