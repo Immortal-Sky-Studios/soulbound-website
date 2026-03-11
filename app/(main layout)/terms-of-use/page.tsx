891bd0eae810a35db24dec7b98f131db5dbb3c24
gracieArmour <graciearmour42@gmail.com>
2026-03-10 19:34:23 -0700

    Minor adjustment to Terms of Use page

--- app/(main layout)/terms-of-use/page.tsx
+++ app/(main layout)/terms-of-use/page.tsx
@@ -1,6 +1,6 @@
 export default function TermsOfUse() {
     return (
-        <main className="flex flex-col justify-center items-center h-[90vh] w-[100vw] overflow-x-hidden bg-[url('/images/backgrounds/CrackedPageBackground.png')] bg-cover">
+        <main className="flex flex-col justify-center items-center h-[90vh] w-[100vw]">
             <h1 className="mb-0">Terms of Use</h1>
             <h2>No GenAI</h2>
             <p className="mb-[2rem]">Training or using. Like ever.</p>
