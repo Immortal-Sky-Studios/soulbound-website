d0c406710a10ccceb9963099ef1c73af95a75b14
gracieArmour <graciearmour42@gmail.com>
2026-03-10 21:09:23 -0700

    Added show/hide function with transitions to announcement component

--- components/Announcement.tsx
+++ components/Announcement.tsx
@@ -1,6 +1,7 @@
 'use client'
-import { use } from 'react';
+import { use, useState } from 'react';
 import { getLatestEp } from '@/lib/neonFunctions';
+
 
 export default function Announcement({
     latestEp,
@@ -11,14 +12,21 @@ export default function Announcement({
 }) {
     const episode = use(latestEp);
     const announcement = use(latestAnnounce);
-    if (episode?.id < announcement?.id) {
-        return (
-            <div id="announcement-container" className="py-[1rem] px-[2rem] whitespace-pre-line bg-[#949494] border-[1rem] border-image-[url('/images/borders/MetalHashBorder.png')] border-slice-t-[980] border-slice-r-[1000] border-slice-b-[990] border-slice-l-[1030] border-image-width-[4rem] border-repeat-round">
-                <h2>Announcement!</h2>
-                <p>{announcement?.description?.replace(/\\n/g,"\n")}</p>
-            </div>
-        )
-    }
+    if (episode?.id > announcement?.id) return (<></>);
 
-    return (<></>)
+    const [open, setOpen] = useState(false);
+
+    function animateOpenClose() {
+        setOpen(!open);
+    }
+    
+    return (
+        <div id="announcement-container" className={`group ${open ? 'open' : 'closed'} py-[1rem] px-[2rem] whitespace-pre-line bg-[#949494] border-[1rem] border-image-[url('/images/borders/MetalHashBorder.png')] border-slice-t-[980] border-slice-r-[1000] border-slice-b-[990] border-slice-l-[1030] border-image-width-[4rem] border-repeat-round`}>
+            <div className="flex flex-row justify-start items-center">
+                <h2>Announcement!</h2>
+                <button onClick={animateOpenClose} className="mb-[0.5rem] ml-[2rem]">{open ? "Hide ⮝" : "Show ⮟"}</button>
+            </div>
+            <p className={`transition-[max-height] duration-[1s] ease-in-out not-group-[.open]:max-h-0 group-[.open]:max-h-[150vh] overflow-y-hidden `}>{announcement?.description?.replace(/\\n/g,"\n")}</p>
+        </div>
+    )
 }
