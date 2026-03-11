60a2cff154f7e8193e8396e1be3ba831c8b74a36
gracieArmour <graciearmour42@gmail.com>
2026-03-10 19:42:22 -0700

    Removed dev console log

--- components/Announcement.tsx
+++ components/Announcement.tsx
@@ -12,7 +12,6 @@ export default function Announcement({
     const episode = use(latestEp);
     const announcement = use(latestAnnounce);
     if (episode?.id < announcement?.id) {
-        console.log(announcement.description)
         return (
             <div id="announcement-container" className="py-[1rem] px-[2rem] whitespace-pre-line bg-[#949494] border-[1rem] border-image-[url('/images/borders/MetalHashBorder.png')] border-slice-t-[980] border-slice-r-[1000] border-slice-b-[990] border-slice-l-[1030] border-image-width-[4rem] border-repeat-round">
                 <h2>Announcement!</h2>
