280eea0cb7c6a37ed0665a61e5e8da2a6fbe5c32
gracieArmour <graciearmour42@gmail.com>
2026-03-10 19:00:19 -0700

    Added skeleton terms of use page

---
+++ app/(main layout)/terms-of-use/page.tsx
@@ -0,0 +1,9 @@
+export default function TermsOfUse() {
+    return (
+        <main className="flex flex-col justify-center items-center h-[90vh] w-[100vw] overflow-x-hidden bg-[url('/images/backgrounds/CrackedPageBackground.png')] bg-cover">
+            <h1 className="mb-0">Terms of Use</h1>
+            <h2>No GenAI</h2>
+            <p className="mb-[2rem]">Training or using. Like ever.</p>
+        </main>
+    )
+}
