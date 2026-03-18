0e87b1fc1f8782a24a00a19b1a076e2cacc01a1e
gracieArmour <graciearmour42@gmail.com>
2026-03-18 16:02:22 -0700

    Rounded and adjusted backtotop button to minimize hitbox conflicts

--- components/BackToTopButton.tsx
+++ components/BackToTopButton.tsx
@@ -23,9 +23,9 @@ export default function BackToTopButton() {
 
     return (
         <>
-            <a id="back-to-top" href="#top" className={`sticky h-fit bottom-5 right-5 z-100 pointer-events-auto ${scrollY == 0 ? 'hidden' : ''}`}>
+            <a id="back-to-top" href="#top" className={`sticky h-fit bottom-5 right-5 z-100 pointer-events-auto rounded-full ${scrollY == 0 ? 'hidden' : ''}`}>
                 <Image
-                    className="object-contain w-[7rem] h-[7rem] sm:w-[5rem] sm:h-[5rem]"
+                    className="object-contain w-[6rem] h-[6rem] lg:w-[5rem] lg:h-[5rem] rounded-full"
                     src={UpArrow}
                     alt="Upwards facing styled arrow"
                 />
