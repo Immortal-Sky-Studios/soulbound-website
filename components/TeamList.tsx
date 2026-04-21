85d8009376535b5d7245bbbe00942ad0f2cf1e78
gracieArmour <graciearmour42@gmail.com>
2026-04-20 22:59:58 -0700

    Minor padding adjustments to teamlist

--- components/TeamList.tsx
+++ components/TeamList.tsx
@@ -50,8 +50,8 @@ export default function TeamList({
                     <div className="flex flex-col justify-start w-full">
                         <div className={`flex flex-col ${direction ? "items-start" : "items-end"}`}>
                             <h2 className={`px-[0.5rem] mb-0 w-full lg:w-3/4 xl:w-5/8 2xl:w-1/2 ${direction ? "text-start" : "text-end"} bg-cyan-300`}>{entry.name} ({entry.pronouns})</h2>
-                            <div className={`h-fit p-[0.25rem] w-3/4 lg:w-5/12 ${direction ? "text-start" : "text-end"} text-[#949494] bg-[#414042]`}>{entry.jobs.length == 0 ? "Upcoming Team Member" : entry.jobs.join(", ")}</div>
-                            {entry.roles.length != 0 && <div className={`h-fit p-[0.25rem] mb-[1rem] w-2/3 lg:w-1/3 ${direction ? "text-start" : "text-end"} text-[#949494] bg-[#525053]`}><b>Roles:</b> {entry.roles.join(", ")}</div>}
+                            <div className={`h-fit py-[0.25rem] px-[0.5rem] w-3/4 lg:w-5/12 ${direction ? "text-start" : "text-end"} text-[#949494] bg-[#414042]`}>{entry.jobs.length == 0 ? "Upcoming Team Member" : entry.jobs.join(", ")}</div>
+                            {entry.roles.length != 0 && <div className={`h-fit py-[0.25rem] px-[0.5rem] mb-[1rem] w-2/3 lg:w-1/3 ${direction ? "text-start" : "text-end"} text-[#949494] bg-[#525053]`}><b>Roles:</b> {entry.roles.join(", ")}</div>}
                         </div>
                         <div className={`flex flex-col justify-start ${direction ? "items-start" : "items-end"} mx-[1rem] pb-[1rem] grow`}>
                             <div className={`w-full max-lg:p-[1rem] mx-[1rem] my-[1rem] ${direction ? "text-start" : "text-end"}`}>
