9d6372c4ac0450e80b7de61e3334e0d4d809e802
gracieArmour <graciearmour42@gmail.com>
2026-04-21 17:50:02 -0700

    Fixed embed button feedback function to correctly use react states

--- components/EmbedButton.tsx
+++ components/EmbedButton.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Image from "next/image";
+import { useState } from "react";
 
 import EmbedIcon from '@images/icons/embed-icon.svg';
 
@@ -10,24 +11,25 @@ export default function EmbedButton({
 }: {
     embedStr: string
 }) {
+    const [label, setLabel] = useState("EMBED");
+
+    function handleClick() {
+        navigator.clipboard.writeText(embedStr);
+        setLabel("COPIED!");
+        setTimeout(() => {
+            setLabel("EMBED");
+        }, 1000);
+    }
+
     return (
         <li>
-            <button className="cursor-pointer" onClick={() => {
-                navigator.clipboard.writeText(embedStr);
-                let label = document.getElementById("embed-label");
-                if (label) {
-                    label.textContent = "COPIED!";
-                    setTimeout(() => {
-                        label.textContent = "EMBED";
-                    }, 1000);
-                }
-            }}>
+            <button className="cursor-pointer" onClick={handleClick}>
                 <Image
                     className="h-[3rem] w-fit"
                     src={EmbedIcon}
                     alt="Embed Icon"
                 />
-                <div id="embed-label" className="-mt-[.5rem]">EMBED</div>
+                <div id="embed-label" className="-mt-[.5rem]">{label}</div>
             </button>
         </li>
     )
