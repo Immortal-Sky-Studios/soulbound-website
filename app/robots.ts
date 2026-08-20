375a00e2009ef2e223da2442a574b591254776ae
gracieArmour <graciearmour42@gmail.com>
2026-08-19 17:35:10 -0700

    Fix directory for robots.txt download

--- app/robots.ts
+++ app/robots.ts
@@ -4,7 +4,7 @@ import { downloadRelease } from "@terascope/fetch-github-release";
 
 export default async function robots(): Promise<MetadataRoute.Robots> {
     if (!fs.existsSync('/tmp/robots.txt')) {
-        const _releaseInfo = await downloadRelease('ai-robots-txt','ai.robots.txt','lib/releaseDownloads/',(release) => (true || release),(asset) => (asset.name == 'robots.txt'));
+        const _releaseInfo = await downloadRelease('ai-robots-txt','ai.robots.txt','/tmp/',(release) => (true || release),(asset) => (asset.name == 'robots.txt'));
     }
     const botsStr = fs.readFileSync('/tmp/robots.txt').toString();
     const aiBots = botsStr.split('\n').map((entry) => entry.split('User-agent: ')[1]).filter((entry) => (entry));
